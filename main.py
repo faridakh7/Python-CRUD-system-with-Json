165086ddad4aa75c0b6969b02dc43195e6cb1660
faridakh <farid.ahmadov97@gmail.com>
2020-08-30 23:25:44 +0400

    CRUD

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from delete import *
 from write import *
 from read import *
 from update import *
+from showalldata import  *
 
 
 def start():
     xususiEmr = int(input("""
     Aşağıdakı Əmrlərdən Birini Secin:
 
-    1.Databasedəki məlumatların əldə olunması
-    2.Databaseye yeni məlumat əlavə edilməsi
-    3.Database üzərində ID görə dəyişikliklərin edilməsi
-    4.Databasedən ID görə məlumatların silinməsi
+    1.Databasedəki məlumatların göstərilməsi
+    2.Databasedəki məlumatların Qiymete ve Ada gore əldə olunması
+    3.Databaseye yeni məlumat əlavə edilməsi
+    4.Database üzərində ID görə dəyişikliklərin edilməsi
+    5.Databasedən ID görə məlumatların silinməsi
+    6.Programi dayandirmaq
     
 Seciminizi Daxil edin:  """))
 
     if xususiEmr == 1:
+        showAllData()
+        start()
+    if xususiEmr == 2:
         choice = int(input("""
         1. Qiymete gore axtaris ucun
         2. Ada gore axtaris ucun
@@ -27,17 +33,23 @@ Seciminizi Daxil edin:  """))
         if choice == 2:
             getProductbyName()
             start()
-    if xususiEmr == 2:
+    if xususiEmr == 3:
         addNewData()
         start()
-    if xususiEmr == 3:
+    if xususiEmr == 4:
         updateDataFromId()
         start()
-    if xususiEmr == 4:
+    if xususiEmr == 5:
         delProductbyName()
         start()
+
+    if xususiEmr == 6:
+        def programbitdi():
+            print("Program muveffeqiyyetle dayandirildi")
+            return
+        programbitdi()
     else:
-        print("Yalnız 1-4 arası bir seçim edə bilərsiniz!")
+        print("Yalnız 1-6 arası bir seçim edə bilərsiniz!")
         start()
 
 start()
---
+++ showalldata.py
@@ -0,0 +1,14 @@
+import json
+
+
+def getDataFormJsonFile(_fileName):
+    with open(_fileName, "r") as connect:
+        return json.load(connect)
+
+
+data = getDataFormJsonFile("db.json")
+
+
+def showAllData():
+    for d in data['products']:
+            print(f"ID:{d['productId']} | Name:{d['productName']} | Price:{d['productPrice']} | Count:{d['productCount']} ")
