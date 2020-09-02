0275d2d435ef12de85ae47a8c1f1e1f3c9c2aedf
faridakh <farid.ahmadov97@gmail.com>
2020-09-02 12:58:36 +0400

    Last Edit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from showalldata import *
 
 
 def start():
-    xususiEmr = int(input("""
+    xususiEmr = input("""
     Aşağıdakı Əmrlərdən Birini Secin:
 
     1.Databasedəki məlumatların göstərilməsi
@@ -16,39 +16,41 @@ def start():
     5.Databasedən ID görə məlumatların silinməsi
     6.Programı dayandırmaq
     
-Seciminizi Daxil edin:  """))
+Seciminizi Daxil edin:  """).strip()
+    if xususiEmr.isnumeric() and 0 < int(xususiEmr) < 7:
+        option = int(xususiEmr)
 
-    if xususiEmr == 1:
-        showAllData()
-        start()
-    if xususiEmr == 2:
-        choice = int(input("""
-        1. Qiymətə gore filter edilməsi
-        2. Ada görə axtarış edilməsi
-        
-        SECIMINIZI DAXIL EDIN : """))
-        if choice == 1:
-            getProductbyPrice()
+        if option == 1:
+            showAllData()
             start()
-        if choice == 2:
-            getProductbyName()
+        if option == 2:
+            choice = int(input("""
+            1. Qiymətə gore filter edilməsi
+            2. Ada görə axtarış edilməsi
+            
+            SECIMINIZI DAXIL EDIN : """))
+            if choice == 1:
+                getProductbyPrice()
+                start()
+            if choice == 2:
+                getProductbyName()
+                start()
+        if option == 3:
+            addNewData()
             start()
-    if xususiEmr == 3:
-        addNewData()
-        start()
-    if xususiEmr == 4:
-        updateDataFromId()
-        start()
-    if xususiEmr == 5:
-        delProductbyName()
-        start()
+        if option == 4:
+            updateDataFromId()
+            start()
+        if option == 5:
+            delProductbyName()
+            start()
 
-    if xususiEmr == 6:
-        def programbitdi():
-            print("Program muveffeqiyyətlə dayandırıldı")
-            return
+        if option == 6:
+            def programbitdi():
+                print("Program muveffeqiyyətlə dayandırıldı")
+                return
 
-        programbitdi()
+            programbitdi()
     else:
         print("Yalnız 1-6 arası bir seçim edə bilərsiniz!")
         start()
